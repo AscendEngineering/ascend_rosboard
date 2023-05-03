86fd660b2d5576337da193a09bf500932c887337
rdars <codrea.eric@gmail.com>
2023-05-03 09:20:33 -0700

    changed ip address

--- rosboard/html/js/viewers/microphoneViewer.js
+++ rosboard/html/js/viewers/microphoneViewer.js
@@ -53,7 +53,7 @@ const mediaStream = navigator.mediaDevices.getUserMedia({ audio: true })
     source.connect(processor);
 
     processor.connect(audioContext.destination);
-    const socket = new WebSocket('wss://192.168.93.128:1234'); // Create a WebSocket to communicate with the Python server
+    const socket = new WebSocket('wss://10.1.10.184:1234'); // Create a WebSocket to communicate with the Python server
 
     socket.onopen = () => {
         console.log("WebSocket connection established");
